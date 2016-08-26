989066e8ac38111a047ee01fb88ddf842ee5256d
Katy Henning <katyannhenning@gmail.com>
2016-08-26 16:12:09 -0700

    add image and css styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,25 @@
 //business
 
-function Pizza(topping, size, addTopping) {
-  this.pTopping = topping;
+function Pizza(topping, size) {
+  this.pTopping = [topping];
   this.pSize = size;
-  this.pAddTopping = addTopping
-}
+  }
 
 Pizza.prototype.cost = function() {
   var pizzaPrice = 10;
-  if (this.pTopping === "Pepperoni" || "Artichoke") {
+  if (this.pTopping === "Pepperoni"){
     pizzaPrice += 3;
   }
-    else if (this.pTopping === "Olives"){
+    else if (this.pTopping === "Artichoke") {
+      pizzaPrice += 2
+  }
+    else if (this.pTopping === "Olives") {
+      pizzaPrice += 1
+    }
+    else if (this.pTopping === "Mushrooms") {
       pizzaPrice += 1;
   }
-    else if (this.pTopping === "Mushrooms"){
-      pizzaPrice += 1;
-  }
-   if (this.pSize === "Small"){
+    if (this.pSize === "Small"){
       pizzaPrice += 5;
   }
     else if (this.pSize === "Medium"){
@@ -26,24 +28,25 @@ Pizza.prototype.cost = function() {
     else if (this.pSize === "Large"){
       pizzaPrice += 10;
   }
-    return pizzaPrice;
-    alert(pizzaPrice);
+ return pizzaPrice;
 }
 
 
 //ui//////////////////////////////////
 $(document).ready(function() {
   $("form").submit(function(event) {
-    event.preventDefault();
-    var allVars = [];
-    var inputtedTopping = $('.checkbox :checked').each(function() {
-        alert($(this).val());
-    }).get();
-      console.log(allVars);
-    var inputtedTopping2 = $("#topping2").val();
-    var inputtedSize = $("#size").val();
 
-    var newPizza = new Pizza(inputtedTopping, inputtedSize);
+    event.preventDefault();
+    var inputtedTopping = $('.chk :checked').each(function() {
+        return($(this).val());
+    }).get();
+    var inputtedSize = $("#size").val();
+    var newPizza = new Pizza([inputtedTopping], inputtedSize);
     $("#total").text(newPizza.cost());
+    $("#totalHeader").show();
+  });
+    $("#thankYou").click(function(event){
+    $("#totalHeader").hide();
+    $("#thankYouMessage").show();
   });
 });
