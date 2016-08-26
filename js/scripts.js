53328df9b20d553e6168208584f833826f7be4b2
Katy Henning <katyannhenning@gmail.com>
2016-08-26 13:53:58 -0700

    constructor and functions so that user input processes into total cost

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,44 @@
-// function Pizza(topping, size, cost) {
-//   this.topping = topping;
-//   this.size = size;
-//   this.cost = cost;
-// }
-//
-// Pizza.prototype.totalcost = function () {
-//
-// }
+//business
 
+function Pizza(topping, size) {
+  this.pTopping = topping;
+  this.pSize = size;
+}
+
+Pizza.prototype.cost = function() {
+  var pizzaPrice = 10;
+  if (this.pTopping === "Pepperoni" || "Artichoke") {
+    pizzaPrice += 3;
+  }
+    else if (this.pTopping === "Olives"){
+      pizzaPrice += 1;
+  }
+    else if (this.pTopping === "Mushrooms"){
+      pizzaPrice += 1;
+  }
+   if (this.pSize === "Small"){
+      pizzaPrice += 5;
+  }
+    else if (this.pSize === "Medium"){
+      pizzaPrice += 7;
+  }
+    else if (this.pSize === "Large"){
+      pizzaPrice += 10;
+  }
+    return pizzaPrice;
+    alert(pizzaPrice);
+}
+
+
+//ui
 $(document).ready(function() {
-  $("#toppingBtn").click(function(event) {
-    
+  $("form").submit(function(event) {
     event.preventDefault();
+    var inputtedTopping = $("#topping").val();
+    var inputtedSize = $("#size").val();
 
-    var inputtedtopping = $("#toppings").val();
-    $("#pizzaToppings").append("<li>" + inputtedtopping + "</li>");
+    var newPizza = new Pizza(inputtedTopping, inputtedSize);
+    $("#paragraph").text(newPizza.cost());
+
   });
-
 });
