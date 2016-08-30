b43cbf12f34331a7e7833242b3718c05879ac4ba
Katy Henning <katyannhenning@gmail.com>
2016-08-30 06:47:44 -0700

    add script so that total is properly calculated

--- js/scripts.js
+++ js/scripts.js
@@ -1,47 +1,51 @@
 //business
 
 function Pizza(topping, size) {
-  this.pTopping = [topping];
+  this.pTopping = topping;
   this.pSize = size;
+  this.price = price;
   }
 
-Pizza.prototype.cost = function() {
+Pizza.prototype.cost = function(price) {
   var pizzaPrice = 10;
-  if (this.pTopping === "Pepperoni"){
-    pizzaPrice += 3;
-  }
-    else if (this.pTopping === "Artichoke") {
-      pizzaPrice += 2
-  }
-    else if (this.pTopping === "Olives") {
-      pizzaPrice += 1
+  for (i=0; i<=7; i++) {
+    if (this.pTopping[i] === "Pepperoni"){
+      pizzaPrice += 3;
     }
-    else if (this.pTopping === "Mushrooms") {
-      pizzaPrice += 1;
-  }
-    if (this.pSize === "Small"){
-      pizzaPrice += 5;
-  }
-    else if (this.pSize === "Medium"){
-      pizzaPrice += 7;
-  }
-    else if (this.pSize === "Large"){
-      pizzaPrice += 10;
-  }
- return pizzaPrice;
+      else if (this.pTopping[i] === "Artichoke") {
+        pizzaPrice += 2
+    }
+      else if (this.pTopping[i] === "Olives") {
+        pizzaPrice += 1
+      }
+      else if (this.pTopping[i] === "Mushrooms") {
+        pizzaPrice += 1;
+    }
+      if (this.pSize[i] === "Small"){
+        pizzaPrice += 5;
+    }
+      else if (this.pSize[i] === "Medium"){
+        pizzaPrice += 7;
+    }
+      else if (this.pSize[i] === "Large"){
+        pizzaPrice += 10;
+    }
+    return this.price =pizzaPrice;
 }
 
+}
 
 //ui//////////////////////////////////
 $(document).ready(function() {
   $("form").submit(function(event) {
-
+    debugger;
     event.preventDefault();
-    var inputtedTopping = $('.chk :checked').each(function() {
-        return($(this).val());
-        }).get();
+    var toppings = [];
+     $.each($("input:checked"), function() {
+       toppings.push($(this).val());
+     });
     var inputtedSize = $("#size").val();
-    var newPizza = new Pizza([inputtedTopping], inputtedSize);
+    var newPizza = new Pizza(toppings, inputtedSize);
     $("#total").text(newPizza.cost());
     $("#totalHeader").show();
   });
