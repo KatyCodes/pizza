9f3c7c36b07a746cc136733d5a6ddefe37bd5626
Katy Henning <katyannhenning@gmail.com>
2016-08-30 10:05:45 -0700

    fixes error in constructor

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,10 @@
 function Pizza(topping, size) {
   this.pTopping = topping;
   this.pSize = size;
-  this.price = price;
+  this.price = 0;
   }
 
-Pizza.prototype.cost = function(price) {
+Pizza.prototype.cost = function() {
   var pizzaPrice = 10;
   for (i=0; i<=7; i++) {
     if (this.pTopping[i] === "Pepperoni"){
@@ -21,19 +21,20 @@ Pizza.prototype.cost = function(price) {
       else if (this.pTopping[i] === "Mushrooms") {
         pizzaPrice += 1;
     }
-      if (this.pSize[i] === "Small"){
+  }
+      if (this.pSize === "Small"){
         pizzaPrice += 5;
     }
-      else if (this.pSize[i] === "Medium"){
+      else if (this.pSize === "Medium"){
         pizzaPrice += 7;
     }
-      else if (this.pSize[i] === "Large"){
+      else if (this.pSize === "Large"){
         pizzaPrice += 10;
     }
     return this.price =pizzaPrice;
 }
 
-}
+
 
 //ui//////////////////////////////////
 $(document).ready(function() {
